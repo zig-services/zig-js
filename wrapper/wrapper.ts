f8bb4656a307c30b43750969715c02163b1a3762
Oliver Bestmann <obest@esailors.de>
2018-01-24 10:35:01 +0000

    Fix indentation

--- wrapper/wrapper.ts
+++ wrapper/wrapper.ts
@@ -30,69 +30,69 @@ function extractConfigParameter(): string {
 function initializeStyle() {
     let style = document.createElement("style");
     style.textContent = `
-            html, body, iframe {
-              width: 100%;
-              height: 100%;
+        html, body, iframe {
+          width: 100%;
+          height: 100%;
+    
+          border: 0;
+          margin: 0;
+          padding: 0;
+        }
         
-              border: 0;
-              margin: 0;
-              padding: 0;
-            }
+        .zig-overlay {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: rgba(0, 0, 0, 0.2);
+        }
+        
+        .zig-alert {
+            position: absolute;
+            z-index: 1;
             
-            .zig-overlay {
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-                background: rgba(0, 0, 0, 0.2);
-            }
+            margin-left: auto;
+            margin-right: auto;
+            left: 0;
+            right: 0;
             
-            .zig-alert {
-                position: absolute;
-                z-index: 1;
-                
-                margin-left: auto;
-                margin-right: auto;
-                left: 0;
-                right: 0;
-                
-                top: 3em;
-                width: 80%;
-                max-width: 20em;
-                background: white;
-                box-shadow: 0 0 0.5em rgba(0, 0, 0, 0.25);
-                padding: 1em;
-                border-radius: 0.25em;
-                
-                color: black;
-                font-family: sans-serif;
-            }
+            top: 3em;
+            width: 80%;
+            max-width: 20em;
+            background: white;
+            box-shadow: 0 0 0.5em rgba(0, 0, 0, 0.25);
+            padding: 1em;
+            border-radius: 0.25em;
             
-            .zig-alert__title {
-                font-size: 1.33em;
-                font-weight: bold;
-                padding-bottom: 0.33em;
-            }
+            color: black;
+            font-family: sans-serif;
+        }
+        
+        .zig-alert__title {
+            font-size: 1.33em;
+            font-weight: bold;
+            padding-bottom: 0.33em;
+        }
+        
+        .zig-alert__text {
+            padding-bottom: 0.33em;
+        }
+        
+        .zig-alert__button {
+            color: black;
+            font-weight: bold;
+            text-decoration: none;
             
-            .zig-alert__text {
-                padding-bottom: 0.33em;
-            }
-            
-            .zig-alert__button {
-                color: black;
-                font-weight: bold;
-                text-decoration: none;
-                
-                float: right;
-                padding: 0.25em;
-            }
-            
-            .zig-alert__button:hover {
-                color: #f08;
-                background: #eee;
-            }
-            `;
+            float: right;
+            padding: 0.25em;
+        }
+        
+        .zig-alert__button:hover {
+            color: #f08;
+            background: #eee;
+        }
+    `;
 
     document.head.appendChild(style);
 }
