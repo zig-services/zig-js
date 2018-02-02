c04af03ed7dff3bb5d7419a34219a67774170c0c
Oliver Bestmann <obest@esailors.de>
2018-02-02 10:34:33 +0000

    Cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,23 @@
-const configs = [
-    {prefix: "zig"},
-    {prefix: "wrapper"},
-    {prefix: "integration"},
-];
+module.exports = {
+    entry: {
+        integration: "./integration/integration.ts",
+        wrapper: "./wrapper/wrapper.ts",
+        zig: "./zig/zig.ts",
+    },
+    output: {
+        filename: './out/[name].js',
+    },
 
-module.exports = configs.map(config => {
-    return {
-        entry: `./${config.prefix}/${config.prefix}.ts`,
-        output: {
-            filename: `out/${config.prefix}.min.js`
-        },
-        resolve: {
-            extensions: [".ts"]
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.ts$/,
-                    loader: "ts-loader",
-                    options: {
-                        configFile: `${config.prefix}/tsconfig.json`,
-                    }
-                }
-            ]
-        }
+    resolve: {
+        extensions: [".ts"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "ts-loader",
+            }
+        ]
     }
-});
+};
