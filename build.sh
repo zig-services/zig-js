4a94267bfe2240cb8f2817a3bc40f9aaed2230f2
Oliver Bestmann <obest@esailors.de>
2018-02-02 10:39:28 +0000

    Cleanup

--- build.sh
+++ build.sh
@@ -7,7 +7,3 @@ npm install
 rm -rf out
 mkdir -p out
 node_modules/.bin/webpack -p
-
-#
-# s3cmd put -P --mime-type="application/javascript" --add-header='Cache-Control:public, no-store, no-cache' *.min.js s3://zig.js/latest/
-#
--- upload.sh
+++ upload.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 ./build-in-docker.sh
 
 if [ -n "$1" ] ; then
