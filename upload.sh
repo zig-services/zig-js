9bd5592fcdba3f3ed1045be9c15e3cd750f445c0
Oliver Bestmann <obest@esailors.de>
2018-02-19 09:29:41 +0000

    Upload for dev only

--- upload.sh
+++ upload.sh
@@ -7,11 +7,16 @@ set -e
 if [ -n "$1" ] ; then
     s3cmd put -P \
         --mime-type="application/javascript; charset=utf8" \
-        --add-header='Cache-Control:public, no-store, no-cache' \
+        --add-header='Cache-Control: public,max-age=31536000,immutable' \
          out/*.min.js s3://zig.js/$1/
+
+    s3cmd put -P \
+        --mime-type="application/javascript; charset=utf8" \
+        --add-header='Cache-Control: public,no-store,no-cache' \
+         out/*.min.js s3://zig.js/latest/
 fi
 
 s3cmd put -P \
     --mime-type="application/javascript; charset=utf8" \
-    --add-header='Cache-Control:public, no-store, no-cache' \
-     out/*.min.js s3://zig.js/latest/
+    --add-header='Cache-Control: private,no-store,no-cache' \
+     out/*.min.js s3://zig.js/dev/
