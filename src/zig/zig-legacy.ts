adcbeb96306847f89f04e3939010af0fbb2c251d
Oliver Bestmann <obest@esailors.de>
2018-02-28 08:25:58 +0000

    Move legacy css into a file.

--- src/zig/zig-legacy.ts
+++ src/zig/zig-legacy.ts
@@ -160,32 +160,7 @@ function patchXMLHttpRequest(gameConfig: IGameConfig) {
 function patchInstantWinGamingStyles() {
     // add script for old instant win gaming games to improve scaling
     // of the background screen
-    injectStyle(`
-        #loader img { 
-          max-width: inherit;
-          width: 100%;
-          max-height: 100%;
-        }
-        #loaderImageHolder img { 
-          -webkit-transform: none;
-          transform: none;
-          position: absolute;
-          top: 0;
-          left: 0;
-        }
-        #loaderImageHolder { 
-          position: absolute;
-          width: 100%; 
-          top: 0;
-          left: 0;
-          -webkit-transform: none;
-          transform: none;
-          max-height: !inherit;
-          height: 100%; 
-        }
-        #loadingBarHolder { z-index: 1; }
-        #loadingBarHolder.loaded { display: none !important; }
-    `);
+    injectStyle(require("./legacy.css"));
 }
 
 function patchInstantWinGamingScripts() {
