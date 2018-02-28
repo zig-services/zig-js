1c2a4d76688b1156e363c3cdb708a508fbd242dd
Oliver Bestmann <obest@esailors.de>
2018-02-28 14:08:34 +0000

    Use lib.zig.services as bucket name

--- upload.sh
+++ upload.sh
@@ -9,25 +9,30 @@ if [ "$1" == "latest" ] ; then
     exit 1
 fi
 
+BUCKET=lib.zig.services
+echo "Using bucket "$BUCKET""
+
+s3cmd mb s3://"$BUCKET"
+
 if [ -n "$1" ] && [ "$1" != "dev" ] ; then
     s3cmd put -P --no-preserve \
         --mime-type="application/javascript; charset=utf8" \
         --add-header='Cache-Control: public,max-age=31536000,immutable' \
-         out/*.min.js s3://zig.js/$1/
+         out/*.min.js s3://"$BUCKET"/$1/
 
 
     s3cmd put -P --no-preserve \
         --mime-type="application/javascript; charset=utf8" \
         --add-header='Cache-Control: public,no-store,no-cache' \
-         out/*.min.js s3://zig.js/latest/
+         out/*.min.js s3://"$BUCKET"/latest/
 fi
 
 s3cmd put -P --no-preserve \
     --mime-type="application/javascript; charset=utf8" \
     --add-header='Cache-Control: private,no-store,no-cache' \
-     out/*.min.js s3://zig.js/dev/
+     out/*.min.js s3://"$BUCKET"/dev/
 
 s3cmd put -P --no-preserve \
     --mime-type="text/html; charset=utf8" \
     --add-header='Cache-Control: private,no-store,no-cache' \
-     debug-page.html s3://zig.js/dev/
+     debug-page.html s3://"$BUCKET"/dev/
