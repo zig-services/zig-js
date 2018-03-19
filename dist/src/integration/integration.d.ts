dcdabff0dc2b7953a8a7bc6bbbeb64bc0d66c5cd
Oliver Bestmann <obest@esailors.de>
2018-03-19 12:33:59 +0000

    Rebuild the libraries.

--- dist/src/integration/integration.d.ts
+++ dist/src/integration/integration.d.ts
@@ -3,7 +3,7 @@ import { Request, Result } from "../_common/request";
 import { IGameConfig } from "../_common/config";
 export declare function zigObserveGame(wrapper: HTMLDivElement, frame: HTMLIFrameElement): MessageClient;
 export declare function includeZigGame(targetSelector: string, url: string, config: IGameConfig): MessageClient;
-export declare function registerHTTPHandlerOnly(frame: HTMLIFrameElement, handler?: (r: Request) => Promise<Result>): void;
+export declare function registerHTTPHandlerOnly(frame: HTMLIFrameElement, handler?: (r: Request) => Promise<Result>): VoidFunction;
 export declare const Zig: {
     include: typeof includeZigGame;
     observe: typeof zigObserveGame;
