74fb83a7767dd6e956eebb95925bf5db71a170e9
Oliver Bestmann <obest@esailors.de>
2018-04-19 12:58:03 +0000

    Release 1.0.3-beta.4

--- build.sh
+++ build.sh
@@ -5,13 +5,11 @@ set -e -o pipefail
 # ensure all dependencies are installed
 npm install
 
+rm -rf dist
+
 # Build the modules and put them under ./dist
 node_modules/.bin/webpack --mode production --progress
 rm -rf dist/typings
 
 # copy package.json for npm to output directory
 cp package.json dist/
-
-# package everything in a .tar.gz archive
-VERSION=$(node -p 'require("./package.json").version')
-tar -vc -C dist/ . | gzip -9 > zig-${VERSION}.tar.gz
