9f823f7da229d67f71ce44b0bf53cf79944af415
Oliver Bestmann <obest@esailors.de>
2018-04-19 15:00:28 +0000

    Release 1.0.5

--- upload.sh
+++ upload.sh
@@ -18,25 +18,25 @@ VERSION_MAJOR=$(node -p "require('semver').major('$VERSION')")-$SUFFIX
 VERSION_MAJOR_MINOR=$(node -p "v=require('semver'); v.major('$VERSION') + '.' + v.minor('$VERSION')")-$SUFFIX
 
 echo "Uploading in version '$VERSION_MAJOR'"
-s3cmd put -q --no-preserve \
+s3cmd put -P -q --no-preserve \
     --mime-type="application/javascript; charset=utf8" \
     --add-header="Cache-Control: public, max-age=60" \
      dist/*.js s3://lib.zig.services/zig/$VERSION_MAJOR/
 
 echo "Uploading in version '$VERSION_MAJOR_MINOR'"
-s3cmd put -q --no-preserve \
+s3cmd put -P -q --no-preserve \
     --mime-type="application/javascript; charset=utf8" \
     --add-header="Cache-Control: public, max-age=60" \
      dist/*.js s3://lib.zig.services/zig/$VERSION_MAJOR_MINOR/
 
 echo "Uploading in version '$VERSION'"
-s3cmd put -q --no-preserve \
+s3cmd put -P -q --no-preserve \
     --mime-type="application/javascript; charset=utf8" \
     --add-header='Cache-Control: public, max-age=60' \
      dist/*.js s3://lib.zig.services/zig/$VERSION/
 
 echo "Uploading debug page"
-s3cmd put -q --no-preserve \
+s3cmd put -P -q --no-preserve \
     --mime-type="text/html; charset=utf8" \
     --add-header='Cache-Control: private,no-store,no-cache' \
      debug-page.html s3://mylotto24.frontend.zig.services/dev/
