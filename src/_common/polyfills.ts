f64ac6a53356249e138eab9f22bf2e8902562aad
Oliver Bestmann <obest@esailors.de>
2018-10-10 12:36:32 +0000

    Add polyfill for .find()

--- src/_common/polyfills.ts
+++ src/_common/polyfills.ts
@@ -1,5 +1,6 @@
 import 'core-js/modules/es6.promise';
 import 'core-js/modules/es6.object.assign';
+import 'core-js/modules/es6.array.find';
 import 'core-js/modules/web.dom.iterable';
 import 'localstorage-browser-polyfill';
 import 'fullscreen-polyfill';
