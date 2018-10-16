12825f2b6f7256b70a1fbe2707da1abcfb3abb56
Oliver Bestmann <obest@esailors.de>
2018-10-16 07:27:26 +0000

    Add a very simple polyfill for localStorage

--- src/_common/polyfills.ts
+++ src/_common/polyfills.ts
@@ -2,8 +2,9 @@ import 'core-js/modules/es6.promise';
 import 'core-js/modules/es6.object.assign';
 import 'core-js/modules/es6.array.find';
 import 'core-js/modules/web.dom.iterable';
-import 'localstorage-browser-polyfill';
+
 import 'fullscreen-polyfill';
+
 import {Logger} from './logging';
 
 const logger = Logger.get('zig.Polyfill');
