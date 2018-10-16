920d41efe8ebe3ee400196e148e85298302a424c
Oliver Bestmann <obest@esailors.de>
2018-10-16 07:25:05 +0000

    Add a very simple polyfill for localStorage

--- src/_common/polyfills.ts
+++ src/_common/polyfills.ts
@@ -4,5 +4,59 @@ import 'core-js/modules/es6.array.find';
 import 'core-js/modules/web.dom.iterable';
 import 'localstorage-browser-polyfill';
 import 'fullscreen-polyfill';
+import {Logger} from './logging';
 
+const logger = Logger.get('zig.Polyfill');
 
+/**
+ * Very simple polyfill for localStorage
+ */
+(function () {
+    let supported = false;
+
+    if (window.hasOwnProperty('localStorage') && window.localStorage !== null) {
+        supported = true;
+
+        // Some browsers will return true when in private browsing mode so test to make sure it's functional.
+        try {
+            const key = 'swxTest_' + Math.round(Math.random() * 1e7);
+
+            // just so no one optimizes the catch away
+            if (key === 'will never match') {
+                // noinspection ExceptionCaughtLocallyJS
+                throw Error();
+            }
+
+            window.localStorage.setItem(key, 'test');
+            window.localStorage.removeItem(key);
+        } catch (e) {
+            logger.warn('localStorage not functional, falling back to session Object.');
+            supported = false;
+        }
+    }
+
+    if (!supported) {
+        let data: { [key: string]: any } = {};
+
+        (window as any).localStorage = {
+            setItem: function (key: string, value: any) {
+                data[key] = value;
+                return data[key];
+            },
+
+            getItem: function (key: string) {
+                return (key in data) ? data[key] : undefined;
+            },
+
+            removeItem: function (key: string) {
+                delete data[key];
+                return undefined;
+            },
+
+            clear: function () {
+                data = {};
+                return data;
+            },
+        };
+    }
+}());
