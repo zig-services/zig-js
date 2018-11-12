432582632505c235dccc4bd62ec39d05b1b7af93
Oliver Bestmann <obest@esailors.de>
2018-11-12 06:57:40 +0000

    Use our own deepClone function that copies the prototype

--- src/common/common.ts
+++ src/common/common.ts
@@ -1,8 +1,6 @@
 import * as deepFreeze from 'deep-freeze';
 import {DeepReadonly} from 'deep-freeze';
 
-import TsDeepCopy from 'ts-deepcopy';
-
 export function sleep(millis: number): Promise<{}> {
     return new Promise<{}>((resolve => window.setTimeout(resolve, millis)));
 }
@@ -12,5 +10,35 @@ export function sleep(millis: number): Promise<{}> {
  * Returns a deep freezed deep copy of the object.
  */
 export function deepFreezeClone<T>(object: T): DeepReadonly<T> {
-    return deepFreeze(TsDeepCopy(object));
+    return deepFreeze(deepClone(object));
+}
+
+/**
+ * Executes deepcopy.
+ */
+export function deepClone<Tp>(input: Tp): Tp {
+    function doDeepClone(input: any): any {
+        const type = typeof input;
+
+        if (input == null || type === 'string' || type === 'number') {
+            return input;
+
+        } else if (input instanceof Date) {
+            return new Date((input as Date).getTime());
+
+        } else if (Array.isArray(input)) {
+            return input.map(doDeepClone) as any;
+
+        } else if (type === 'object') {
+            const copy: any = {};
+            Object.keys(copy).forEach(key => copy[key] = doDeepClone(input[key]));
+            copy.prototype = input.prototype;
+            return copy;
+
+        } else {
+            return input;
+        }
+    }
+
+    return doDeepClone(input);
 }
